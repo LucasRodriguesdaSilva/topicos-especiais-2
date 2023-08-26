db682f6ffb547b20e99f75453dbf8da6a33282c5
Lucas Rodrigues <lucasrodrigues@sobral.gov.ce.br>
2023-08-26 14:27:19 -0300

    feat: Adicionado funções de mensagens para o usuário

---
+++ Trabalho-Esquenta/utils/output_info.py
@@ -0,0 +1,5 @@
+def mensagemInicial():
+    print(f'Iniciando ...')
+
+def imprimirInfo(nome_algoritmo,nome_instancia,tipo_lista,num_loops):
+    print(f'Algoritmo: {nome_algoritmo} - Instancia: {nome_instancia} - Tipo da Lista: {tipo_lista} - Repetições: {num_loops}')
