4d519d3f360a7dcc46765751af26d107321e11ba
Lucas Rodrigues <lucasrodrigues@sobral.gov.ce.br>
2023-08-26 14:24:14 -0300

    feat: adicionado medição de tempo de execução

--- Trabalho-Esquenta/buscaLinearV1/busca_linear_v1.py
+++ Trabalho-Esquenta/buscaLinearV1/busca_linear_v1.py
@@ -1,8 +1,14 @@
+import time
 class BuscaLinearV1:
     @staticmethod
     def busca_linear_v1(x, v):
+        inicio = time.time()
         indice = -1
         for i in range(len(v)):
             if v[i] == x:
                 indice = i
-        return indice
+        
+        fim = time.time()
+        tempo_execucao = fim - inicio
+
+        return indice, tempo_execucao
