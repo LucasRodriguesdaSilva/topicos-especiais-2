be0adae97aaa45c6bd72d4f68d3d4e003965af14
Lucas Rodrigues <lucasrodrigues@sobral.gov.ce.br>
2023-08-26 14:26:27 -0300

    feat: Adicionado transformação do arquivo para vetor de inteiros

--- Trabalho-Esquenta/utils/ler_arquivos.py
+++ Trabalho-Esquenta/utils/ler_arquivos.py
@@ -1,12 +1,20 @@
 import os 
 
-class LerArquivos:
-    @staticmethod
-    def ler_arquivo(caminho_relativo):
-        diretorio_atual = os.path.dirname(os.path.abspath(__file__))
-        caminho_arquivo = os.path.join(diretorio_atual, caminho_relativo)
+def ler_arquivo(caminho_relativo):
+    diretorio_atual = os.path.dirname(os.path.abspath(__file__))
+    caminho_arquivo = os.path.join(diretorio_atual, caminho_relativo)
 
-        with open(caminho_arquivo, 'r') as arquivo:
-            conteudo = arquivo.read()
+    with open(caminho_arquivo, 'r') as arquivo:
+        conteudo = arquivo.read()
+    
+    linhas = conteudo.split('\n')
+    vetor = []
 
-        return conteudo
+    for linha in linhas:
+        numeros = linha.split(',')
+        for numero_str in numeros:
+            if numero_str.strip():
+                numero = int(numero_str)
+                vetor.append(numero)
+    
+    return vetor
