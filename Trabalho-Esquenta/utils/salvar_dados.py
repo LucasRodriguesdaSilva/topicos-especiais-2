741ba9e0862109e463bf1946f35f0bd50e168a10
Lucas Rodrigues <lucasrodrigues@sobral.gov.ce.br>
2023-08-26 14:27:52 -0300

    feat: Salvamento dos dados em um arquivo .csv

---
+++ Trabalho-Esquenta/utils/salvar_dados.py
@@ -0,0 +1,28 @@
+import os
+import csv
+
+def salvarInformacoes(dicionario,caminhoAbsoluto, pasta):
+    nomeArquivo = 'dados.csv'
+    caminho_output = os.path.join(pasta, 'output', 'dados')
+    caminho_completo = os.path.join(caminhoAbsoluto, caminho_output, nomeArquivo)
+
+    if not os.path.exists(caminho_output):
+        os.makedirs(caminho_output)
+    
+    with open(caminho_completo, 'w', newline='') as arquivo:
+        colunas = ["Iteracao", "Uso de Memoria - Inicial (bytes)", 'Uso de Memoria - Final (bytes)', 'Uso de Memoria - Diferenca (bytes)', "Tempo de Execucao (s)", "Valor Procurado", "Indice no Vetor"] 
+        escritor_csv = csv.DictWriter(arquivo, fieldnames=colunas)
+        escritor_csv.writeheader()
+        
+        for iteracao, dados in dicionario.items():
+            escritor_csv.writerow({
+                "Iteracao": iteracao,
+                "Uso de Memoria - Inicial (bytes)": dados["uso_inicial"],
+                "Uso de Memoria - Final (bytes)": dados["uso_final"],
+                "Uso de Memoria - Diferenca (bytes)": dados["uso_diferenca"],
+                "Tempo de Execucao (s)": dados["tempo_execucao"],
+                "Valor Procurado": dados['valor_procurado'] , 
+                "Indice no Vetor": dados['resultado_encontrado']
+            })
+
+    print(f'Dados salvos em {caminho_completo}')
