5e8ee832f383009cdcf918bda0e9c64ebc951e5a
Lucas Rodrigues <lucasrodrigues@sobral.gov.ce.br>
2023-08-27 19:20:18 -0300

    feat: Adicionado número de busca para o usuário

--- Trabalho-Esquenta/utils/output_info.py
+++ Trabalho-Esquenta/utils/output_info.py
@@ -1,5 +1,5 @@
 def mensagemInicial():
     print(f'Iniciando ...')
 
-def imprimirInfo(nome_algoritmo,nome_instancia,tipo_lista,num_loops):
-    print(f'Algoritmo: {nome_algoritmo} - Instancia: {nome_instancia} - Tipo da Lista: {tipo_lista} - Repetições: {num_loops}')
+def imprimirInfo(nome_algoritmo,nome_instancia,tipo_lista,num_loops, valor_procurado):
+    print(f'Algoritmo: {nome_algoritmo} - Instancia: {nome_instancia} - Tipo da Lista: {tipo_lista} - Repetições: {num_loops} - Nº Procurado: {valor_procurado}')
