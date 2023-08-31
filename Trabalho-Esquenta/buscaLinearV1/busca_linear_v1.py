e63022f2c9e5dedcb5fd615cb274d3f18f2ad8e7
Lucas Rodrigues <lucasrodrigues@sobral.gov.ce.br>
2023-08-30 23:26:52 -0300

    fix: Corrigido forma de calcular o tempo

--- Trabalho-Esquenta/buscaLinearV1/busca_linear_v1.py
+++ Trabalho-Esquenta/buscaLinearV1/busca_linear_v1.py
@@ -2,13 +2,9 @@ import time
 class BuscaLinearV1:
     @staticmethod
     def busca_linear_v1(x, v):
-        inicio = time.time()
         indice = -1
         for i in range(len(v)):
             if v[i] == x:
                 indice = i
-        
-        fim = time.time()
-        tempo_execucao = fim - inicio
-
-        return indice, tempo_execucao
+                
+        return indice
--- Trabalho-Esquenta/buscaLinearV2/busca_linear_v2.py
+++ Trabalho-Esquenta/buscaLinearV2/busca_linear_v2.py
@@ -2,13 +2,8 @@ import time
 class BuscaLinearV2:
     @staticmethod
     def busca_linear_v2(x, v):
-        inicio = time.time()
         for i in range(len(v)):
             if v[i] == x:
-                fim = time.time()
-                tempo_execucao = fim - inicio
-                return i, tempo_execucao
-
-        fim = time.time()
-        tempo_execucao = fim - inicio
-        return -1, tempo_execucao
+                return i
+                
+        return -1
